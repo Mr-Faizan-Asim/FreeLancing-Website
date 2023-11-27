53fef1a9f5d82410b3f65a776528890da52504ed
PC <mr.faizan.asim@gmail.comgit config --global user.email mr.faizan.asim@gmail.com>
2023-11-28 03:46:20 +0500

    DB Connected

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const dotenv = require('dotenv')
 const app = require('./app.js')
-
+const connectDatabase = require('./db/database.js')
 
 dotenv.config({path:"config/config.env"})
+
+connectDatabase()
+
 
 const server = app.listen(process.env.PORT,()=>{
     console.log("server is working on " + process.env.PORT)
